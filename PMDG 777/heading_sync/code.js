0a4d41a2c3d63efba9e2594b4f534f812f978546
Joseph Yoon <joseph@jyoon.pro>
2024-07-13 16:21:23 +0900

    Add heading scroll acceleration due to PMDG update

--- PMDG 777/heading_sync/code.js
+++ PMDG 777/heading_sync/code.js
@@ -1,5 +1,29 @@
 let isUserScrolling, scrollIdleTimeout;
 
+// Account for PMDG knob acceleration
+function getStepCount(target) {
+  let normalStep, extraStep;
+
+  if (target <= 4) {
+    normalStep = target;
+    extraStep = 0;
+  } else if (target <= 14) {
+    normalStep = 4 + Math.floor((target - 4) / 2);
+    extraStep = target % 2;
+  } else if (target <= 29) {
+    normalStep = 9 + Math.floor((target - 14) / 3);
+    extraStep = (target - 14) % 3;
+  } else if (target <= 49) {
+    normalStep = 14 + Math.floor((target - 29) / 4);
+    extraStep = (target - 29) % 4;
+  } else {
+    normalStep = 19 + Math.floor((target - 49) / 5);
+    extraStep = (target - 49) % 5;
+  }
+
+  return {normalStep, extraStep};
+}
+
 function getSignedAngleDifference(a, b) {
   const angle = ((a - b) % 360 + 540) % 360 - 180;
   return angle < -180 ? angle + 360 : angle;
@@ -17,13 +41,27 @@ const setMcpHeading = async (targetHeading, retry) => {
   const angleDifference = getSignedAngleDifference(mcpHeading, targetHeading);
 
   if (angleDifference < 0) {
-    for (let i = 0; i < -angleDifference; i++) {
+    const {normalStep, extraStep} = getStepCount(-angleDifference);
+
+    for (let i = 0; i < normalStep; i++) {
       await timeout(50);
       this.$api.variables.set("K:ROTOR_BRAKE", "number", 218007);
     }
+
+    for (let i = 0; i < extraStep; i++) {
+      await timeout(700);
+      this.$api.variables.set("K:ROTOR_BRAKE", "number", 218007);
+    }
   } else if (angleDifference > 0) {
-    for (let i = 0; i < angleDifference; i++) {
+    const {normalStep, extraStep} = getStepCount(angleDifference);
+
+    for (let i = 0; i < normalStep; i++) {
       await timeout(50);
+      this.$api.variables.set("K:ROTOR_BRAKE", "number", 218008);
+    }
+
+    for (let i = 0; i < extraStep; i++) {
+      await timeout(700);
       this.$api.variables.set("K:ROTOR_BRAKE", "number", 218008);
     }
   }
